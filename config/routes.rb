347387c1ce2016243a06075b7405373b9a44efda
Tomas Doherty <tomasdoh1@gmail.com>
2019-01-23 17:45:20 +0000

    Created APIVerison file and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :pets
+  end
 end

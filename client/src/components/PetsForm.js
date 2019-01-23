588e930adabb96b87147062d1f44dd28808dc73b
Ijeoma Nelson <ijeoma.nelson1@gmail.com>
2019-01-23 10:55:42 +0000

    created a form for uploading pets

---
+++ client/src/components/PetsForm.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react';
+
+class PetForm extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+}

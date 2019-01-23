2abeb865a20e8c2151675bc23658460eb84b7f2a
Ijeoma Nelson <ijeoma.nelson1@gmail.com>
2019-01-23 12:34:56 +0000

    added a component for form

--- client/src/components/PetsForm.js
+++ client/src/components/PetsForm.js
@@ -1,9 +1,34 @@
-import React, {Component} from 'react';
-import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react';
+class PetsForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
 
-class PetForm extends Component {
-  constructor() {
-    super();
-    this.state = {}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A Pet was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pet:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
+
+export default PetsForm;

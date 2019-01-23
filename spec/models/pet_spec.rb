e8dfe9aa58956f795e2757d48a258657de0f4178
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-23 11:44:32 +0000

    user can add a tag to a pet

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Pet, type: :model do
   it { should validate_presence_of (:name) }
   it { should validate_presence_of (:owner) }
   it { should validate_presence_of (:description) }
+  it { should validate_presence_of (:tag) }
 end

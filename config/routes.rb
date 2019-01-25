cdb371937b5164aa16c15efdfe72b40887aa4209
Tomas Doherty <tomasdoh1@gmail.com>
2019-01-25 16:06:57 +0000

    Created conversation and message channels

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :pets
 
+    resources :conversations, only: [:index, :create]
+    resources :messages, only: [:create]
+    mount ActionCable.server => '/cable'
+
 end

143beb0db834a0c826b601705097185e9a206f76
Tomas Doherty <tomasdoh1@gmail.com>
2019-01-25 13:36:08 +0000

    Added ReactActiveStorage

--- client/src/components/PetsForm.js
+++ client/src/components/PetsForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Col, Row, Button, Form, FormGroup, Label, Input, Container, FormText } from 'reactstrap';
-import GeoAutoComplete from './GeoAutoComplete'
+import GeoAutoComplete from './GeoAutoComplete';
+import ActiveStorageProvider from 'react-activestorage-provider';
 
 
 class PetsForm extends Component {
@@ -65,6 +66,46 @@ class PetsForm extends Component {
             <Label for="location">Last Seen Location</Label>
             <GeoAutoComplete />
           </FormGroup>
+
+          <ActiveStorageProvider
+          endpoint={{
+            path: '/pets',
+            model: 'pet',
+            attribute: 'photo',
+            method: 'POST',
+            }}
+          onSubmit={pet => this.setState({ photo: pet.photo })}
+          render={({ handleUpload, uploads, ready }) => (
+            <div>
+              <input
+                type="file"
+                disabled={!ready}
+                onChange={e => handleUpload(e.currentTarget.files)}
+              />
+
+              {uploads.map(upload => {
+                switch (upload.state) {
+                  case 'waiting':
+                    return <p key={upload.id}>Waiting to upload {upload.file.name}</p>
+                  case 'uploading':
+                    return (
+                      <p key={upload.id}>
+                        Uploading {upload.file.name}: {upload.progress}%
+                      </p>
+                    )
+                  case 'error':
+                    return (
+                      <p key={upload.id}>
+                        Error uploading {upload.file.name}: {upload.error}
+                      </p>
+                    )
+                  case 'finished':
+                    return <p key={upload.id}>Finished uploading {upload.file.name}</p>
+                }
+              })}
+            </div>
+          )}
+        />
 
           <Button color="dark" style={{marginTop: '2rem'}} block>Report</Button>
         </Form>

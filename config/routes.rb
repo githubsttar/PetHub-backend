22816f258873f05e90c2f80436ec2ff769976d93
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-29 14:54:33 +0000

    fixing route for rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :pets
+    resources :users
 
     resources :conversations, only: [:index, :create]
     resources :messages, only: [:create]
     mount ActionCable.server => '/cable'
 
     post   'users/login', to: 'users#login'
-    post   'users/create', to:'users#create'
 end

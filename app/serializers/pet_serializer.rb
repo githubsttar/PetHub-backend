11e43a953d01661d811ccdf061b3407670e75bb9
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-30 17:07:52 +0000

    exporting user id with json response

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -2,11 +2,6 @@ class PetSerializer < ActiveModel::Serializer
 
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :owner, :description, :tag, :location, :created_at, :fileBase64, :fileContentType
-  
-  def picture
-    object.picture.attached?
-    rails_blob_path(object.picture, only_path: true) if object.picture.attached?
-  end
+  attributes :id, :name, :owner, :description, :tag, :user_id, :location, :created_at, :fileBase64, :fileContentType
 
 end

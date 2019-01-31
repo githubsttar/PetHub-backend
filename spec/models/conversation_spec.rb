e2c04ee2a190abf30c34723bf4cc099ce8a0ea4f
Tomas Doherty <tomasdoh1@gmail.com>
2019-01-31 11:35:20 +0000

    Adding conversation and message model specs

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Conversation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(Conversation.new).to be_valid
+  end
+
+  # it { should validate_presence_of(:message) } 
 end
